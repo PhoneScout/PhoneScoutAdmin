08a81abf62e22a969cc0b8e306d2c1e0157a8edb
Pocsai Gergő <pocsaig1@kkszki.hu>
2026-01-28 11:54:33 +0100

    Admin felület WPF
    Felhasználók és Alkatrészek lekérése + xaml.

--- PhoneScoutAdmin/Home.xaml.cs
+++ PhoneScoutAdmin/Home.xaml.cs
@@ -55,14 +55,72 @@ namespace PhoneScoutAdmin
         public string manufacturerEmail { get; set; }
     }
 
+    public class User
+    {
+
+        [JsonPropertyName("userFullName")]
+        public string fullName { get; set; }
+
+        [JsonPropertyName("userEmail")]
+        public string email { get; set; }
+
+        [JsonPropertyName("userPrivilegeLevel")]
+        public int privilegeLevel { get; set; }
+
+        [JsonPropertyName("userPrivilegeName")]
+        public string privilegeName { get; set; }
+
+        [JsonPropertyName("userActive")]
+        public int isActive { get; set; }
+    }
+    public class Storage
+    {
+
+        [JsonPropertyName("partName")]
+        public string partName { get; set; }
+
+        [JsonPropertyName("partAmount")]
+        public int partAmount { get; set; }
+    }
+    public class ComboItem
+    {
+        public int Id { get; set; }
+        public int Level { get; set; }
+        public string Name { get; set; }
+
+        public override string? ToString()
+        {
+            return $"{Level} - {Name}";
+        }
+    }
+
+
     public partial class Home : Window
     {
         string selectedMenu = "phones";
         public ObservableCollection<Phone> phones { get; set; } = new ObservableCollection<Phone>();
         public ObservableCollection<Manufacturer> manufacturers { get; set; } = new ObservableCollection<Manufacturer>();
+        public ObservableCollection<User> users { get; set; } = new ObservableCollection<User>();
+        public ObservableCollection<Storage> parts { get; set; } = new ObservableCollection<Storage>();
+
+        public ObservableCollection<ComboItem> Items { get; } =
+    new ObservableCollection<ComboItem>
+    {
+        new ComboItem { Id = 1, Level = 1, Name = "User" },
+        new ComboItem { Id = 2, Level = 2, Name = "Manufacturer" },
+        new ComboItem { Id = 3, Level = 3, Name = "Admin" },
+    };
+        public ComboItem SelectedItem { get; set; }
+
+
+
+
+
+
         public Home()
         {
             InitializeComponent();
+            DataContext = this;
         }
 
 
@@ -85,6 +143,8 @@ namespace PhoneScoutAdmin
         {
             phoneDataGrid.SelectedItem = null;
             manufacturerDataGrid.SelectedItem = null;
+            userDataGrid.SelectedItem = null;
+
             selectedMenu = "phone";
             using HttpClient client = new HttpClient();
             try
@@ -125,6 +185,8 @@ namespace PhoneScoutAdmin
         {
             phoneDataGrid.SelectedItem = null;
             manufacturerDataGrid.SelectedItem = null;
+            userDataGrid.SelectedItem = null;
+
             selectedMenu = "manufacturer";
 
             using HttpClient client = new HttpClient();
@@ -162,7 +224,91 @@ namespace PhoneScoutAdmin
                 MessageBox.Show("Error: " + ex.Message);
             }
         }
+        private async void loadUsers(object sender, RoutedEventArgs e)
+        {
+            phoneDataGrid.SelectedItem = null;
+            manufacturerDataGrid.SelectedItem = null;
+            userDataGrid.SelectedItem = null;
+            selectedMenu = "user";
 
+            using HttpClient client = new HttpClient();
+            try
+            {
+                string url = "http://localhost:5175/api/wpfUsers";
+                var response = await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = await response.Content.ReadAsStringAsync();
+
+                    var userList = JsonSerializer.Deserialize<List<User>>(json);
+
+                    if (userList != null)
+                    {
+                        users.Clear();
+                        foreach (var user in userList)
+                        {
+                            users.Add(user);
+                        }
+                    }
+
+                    userDataGrid.ItemsSource = userList;
+                    populateInformationPart();
+
+                }
+                else
+                {
+                    MessageBox.Show("Failed to load users from API");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+        private async void loadStorage(object sender, RoutedEventArgs e)
+        {
+            phoneDataGrid.SelectedItem = null;
+            manufacturerDataGrid.SelectedItem = null;
+            userDataGrid.SelectedItem = null;
+            storageDataGrid.SelectedItem = null;
+            selectedMenu = "storage";
+
+            using HttpClient client = new HttpClient();
+            try
+            {
+                string url = "http://localhost:5175/api/wpfStorage";
+                var response = await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = await response.Content.ReadAsStringAsync();
+
+                    var partList = JsonSerializer.Deserialize<List<Storage>>(json);
+
+                    if (partList != null)
+                    {
+                        partList.Clear();
+                        foreach (var part in partList)
+                        {
+                            partList.Add(part);
+                        }
+                    }
+
+                    storageDataGrid.ItemsSource = partList;
+                    populateInformationPart();
+
+                }
+                else
+                {
+                    MessageBox.Show("Failed to load users from API");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
 
 
 
@@ -172,11 +318,29 @@ namespace PhoneScoutAdmin
             {
                 manufacturerDetails.Visibility = Visibility.Visible;
                 phoneDetails.Visibility = Visibility.Collapsed;
+                userDetails.Visibility = Visibility.Collapsed;
+                storageDetails.Visibility = Visibility.Collapsed;
             }
             else if (selectedMenu == "phone")
             {
-                manufacturerDetails.Visibility = Visibility.Collapsed;
                 phoneDetails.Visibility = Visibility.Visible;
+                userDetails.Visibility = Visibility.Collapsed;
+                manufacturerDetails.Visibility = Visibility.Collapsed;
+                storageDetails.Visibility = Visibility.Collapsed;
+            }
+            else if (selectedMenu == "user")
+            {
+                userDetails.Visibility = Visibility.Visible;
+                manufacturerDetails.Visibility = Visibility.Collapsed;
+                phoneDetails.Visibility = Visibility.Collapsed;
+                storageDetails.Visibility = Visibility.Collapsed;
+            }
+            else if (selectedMenu == "storage")
+            {
+                userDetails.Visibility = Visibility.Collapsed;
+                manufacturerDetails.Visibility = Visibility.Collapsed;
+                phoneDetails.Visibility = Visibility.Collapsed;
+                storageDetails.Visibility = Visibility.Visible;
             }
         }
 
@@ -215,9 +379,36 @@ namespace PhoneScoutAdmin
                     manufacturerEmail.Text = "";
                 }
             }
+            if (selectedMenu == "user")
+            {
+                if (userDataGrid.SelectedItem is User selectedUser)
+                {
+                    userName.Text = selectedUser.fullName;
+                    userEmail.Text = selectedUser.email;                    
+                    userPrivilege.SelectedValue = selectedUser.privilegeLevel;
+                    userActive.IsChecked = selectedUser.isActive == 1;
+                }
+                else
+                {
+                    userName.Text = "";
+                    userEmail.Text = "";
+                    userPrivilege.SelectedValue = 1;
+                    userActive.IsChecked = false;
+                }
+            }
+            if (selectedMenu == "storage")
+            {
+                if (userDataGrid.SelectedItem is Storage selectedPart)
+                {
+                    partName.Text = selectedPart.partName;
+                    partAmount.Text = selectedPart.partAmount.ToString();
+                }
+                else
+                {
+                    partName.Text = "";
+                    partAmount.Text = "";
+                }
+            }
         }
-
-
-
     }
 }
